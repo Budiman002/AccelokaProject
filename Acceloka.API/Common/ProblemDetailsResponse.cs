726023e4fbd80c78ca460ee6c2f65dbfb0bc8e89
Arif Budiman <arifb5624@gmail.com>
2026-02-10 09:27:40 +0700

    Checkpoint 2 & 3: Setup MediatR + FluentValidation infrastructure

---
+++ Acceloka.API/Common/ProblemDetailsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Acceloka.API.Common
+{
+    public class ProblemDetailsResponse
+    {
+        public string Type { get; set; } = "https://tools.ietf.org/html/rfc7807";
+        public string Title { get; set; } = string.Empty;
+        public int Status { get; set; }
+        public Dictionary<string, string[]> Errors { get; set; } = new();
+    }
+}
--- Acceloka.API/Program.cs
+++ Acceloka.API/Program.cs
@@ -1,43 +1,56 @@
+using Acceloka.API.Common;
 using Acceloka.API.Data;
+using FluentValidation;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Database
 builder.Services.AddDbContext<AccelokaDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// MediatR (CQRS)
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+});
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/openapi/v1.json", "Acceloka API v1");
 });
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+// Seed database
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<AccelokaDbContext>();
     DbSeeder.SeedData(context);
 }
-
 
 app.Run();
